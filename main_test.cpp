bc5294168a11baf8e201e7684aa310545941917b
Plyaskina Aleksandra <plyaskina-sasha@mail.ru>
2018-06-13 18:22:39 +0700

    Creating multiplication and division tests.

---
+++ main_test.cpp
@@ -0,0 +1,57 @@
+#define GTEST_MAIN
+
+#include <func.h>
+#include <gtest/gtest.h>
+
+TEST(Multipl, test1) {
+    double result = multipl(22,40);
+    double expected = 880;
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Multipl, test2) {
+    double result = multipl(-10,-12.1);
+    double expected = 121;
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Multipl, test3) {
+    double result = multipl(32,-0.5);
+    double expected = -16;
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Multipl, test4) {
+    double result = multipl(0,-121);
+    double expected = 0;
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Division, test1) {
+    double result = division(0,1002);
+    double expected = 0;
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Division, test2) {
+    double result = division(360,6);
+    double expected = 60;
+    ASSERT_EQ(expected, result);
+}
+
+TEST(Division, test3) {
+    double result = division(155,2);
+    double expected = 77.5;
+    ASSERT_DOUBLE_EQ(expected, result);
+}
+
+TEST(Division, test4) {
+    double result = division(0.48,0.3);
+    double expected = 1.6;
+    ASSERT_DOUBLE_EQ(expected, result);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
